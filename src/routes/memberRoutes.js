535aafe4bc9694b0582b96e9e96d82ad63519815
Antonio Neto <antonioolimpiodesaneto@gmail.com>
2026-07-17 19:09:54 -0300

    feat(member): adiciona rota de solicitacao

--- src/routes/memberRoutes.js
+++ src/routes/memberRoutes.js
@@ -2,10 +2,16 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware.js");
 const MemberController = require("../controllers/MemberController.js");
+const MemberAssociationController = require("../controllers/MemberAssociationController.js");
 
 const router = express.Router();
 
 router.get("/member/summary", authMiddleware, MemberController.getMemberSummary);
 router.get("/member/plans", authMiddleware, MemberController.getMemberPlans);
+router.post(
+  "/member/association-request",
+  authMiddleware,
+  MemberAssociationController.createAssociationRequest
+);
 
 module.exports = router;
